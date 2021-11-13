41a1f5e570f229c23a7596cf90bd1791a4fa3059
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2021-11-13 18:52:32 +0530

    Added Profile Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Opportunities from './Components/Opportunities';
+import Profile from './Components/Profile';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/opportunities" element={<Opportunities />} />
+        <Route exact path="/myprofile" element={<Profile/>}/>
       </Routes>
     </Router>
   );
