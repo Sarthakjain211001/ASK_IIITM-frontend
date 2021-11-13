22f4a3c7f7b72d7a4d496bf4bff7868782f6f545
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2021-11-13 19:09:14 +0530

    Added a particular question's Page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Navbar from './Components/Navbar';
 import Opportunities from './Components/Opportunities';
 import Profile from './Components/Profile';
 import Qna from './Components/Qna';
+import Question_page from './Components/Question_page';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+      <Route exact path="/" element={<Qna/>}/>
+      <Route exact path="/question" element={ <Question_page/>} />
         <Route exact path="/opportunities" element={<Opportunities />} />
         <Route exact path="/myprofile" element={<Profile/>}/>
-        <Route exact path="/" element={<Qna/>}/>
+        
+        
       </Routes>
     </Router>
   );
